5e378b19b758eb5b57e80c9d952753ab8282b3a7
francisco ximenes <fximenessilva@gmail.com>
2021-02-02 15:41:06 +0100

    added styles. list styles almost ready

--- frontend/src/components/PostList/PostList.jsx
+++ frontend/src/components/PostList/PostList.jsx
@@ -1,21 +1,24 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './PostList.css';
 import { connect } from 'react-redux';
 import requestPostList from '../../redux/actions/postListAction';
 
 function PostList({ postList, dispatch }) {
+  const [commentInput, setCommentInput] = useState('');
   useEffect(() => {
     if (!postList || !postList?.length) {
       dispatch(requestPostList());
-      console.log(postList);
     }
     if (postList) {
       console.log(postList);
     }
   });
+
   return (
     <div className="article-post-wrapper">
       {postList && postList.length
@@ -29,7 +32,7 @@ function PostList({ postList, dispatch }) {
             <img className="post-profile-pic" src="https://the7.io/digital-agency/wp-content/uploads/sites/46/2018/02/gradient-slider.jpg" alt="" />
           </div>
           <div className="post-profile-user-wrapper">
-            <h4 className="post-username">Alexandre Frota</h4>
+            <h4 className="post-username">alexandre_frota</h4>
           </div>
         </header>
         <div className="post-img-wrapper">
@@ -39,7 +42,11 @@ function PostList({ postList, dispatch }) {
           <span className="material-icons-outlined post-icon">
             favorite
           </span>
-          <span className="material-icons-outlined post-icon">
+          <span
+            role="button"
+            onClick={() => document.getElementById('post-comnt-txtarea').focus()}
+            className="material-icons-outlined post-icon"
+          >
             add_comment
           </span>
         </div>
@@ -49,21 +56,33 @@ function PostList({ postList, dispatch }) {
         </div>
         <div className="post-userProfile-description-wrapper">
 
-          <h4>Alexandre Frota</h4>
+          <h4 className="post-author">alexandre_frota</h4>
           <p className="post-description">{post.description}</p>
         </div>
         <div className="hashtag-wrapper">
           {post.hashtags && post.hashtags.map((hashtag) => <p className="post-hashtag">{hashtag}</p>)}
         </div>
         <div className="post-comments-wrapper">
-          {post.comments && post.comments.map((comment) => <p className="post-comments">{comment}</p>)}
+          {post.comments && post.comments.map((comment) => (
+            <div className="post-author-comment">
+              {' '}
+              <ul className="post-usr-commentd">user1</ul>
+              <ul className="post-comments">{comment}</ul>
+            </div>
+          ))}
         </div>
         <div className="post-comment-input-btn-wrapper">
-          <input type="text" className="post-comment-input" placeholder="Add your comment..." />
+          <textarea
+            type="text"
+            className="post-comment-input"
+            id="post-comnt-txtarea"
+            placeholder="Add your comment..."
+            onChange={(e) => setCommentInput(e.target.value)}
+          />
           <button
             type="button"
             className="post-comment-btn"
-            disabled
+            disabled={!commentInput}
           >
             Publish
           </button>
